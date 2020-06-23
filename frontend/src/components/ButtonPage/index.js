825d9493a892d697941ff05684b6abe6cdfe9165
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-23 20:09:31 -0300

    Criação dos botões usados no NavBar

---
+++ frontend/src/components/ButtonPage/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const ButtonPage = styled.button`
+  height: 40px;
+  width: 150px;
+  margin: auto 10px auto 10px;
+  border-radius: 8px;
+  border-style: none;
+  cursor: pointer;
+  text-align: center;
+`
+
+export default ButtonPage;
