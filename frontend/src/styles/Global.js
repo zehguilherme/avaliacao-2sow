0e2f4f1372de473837597dc0840da31b4e1e4d92
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-25 19:45:46 -0300

    Adição do seletor '*'

--- frontend/src/styles/Global.js
+++ frontend/src/styles/Global.js
@@ -1,13 +1,8 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
 
-  /* http://meyerweb.com/eric/tools/css/reset/
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-
-* {
+body {
   font-family: 'Roboto', sans-serif;
 }
 
