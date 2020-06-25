140b157cb19c7c54846338602c6495c61a81b71d
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-25 19:43:22 -0300

    Criação do botão de Logout

---
+++ frontend/src/components/LogoutButton/index.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const LogoutButton = styled.button`
+  height: 30px;
+  width: 90px;
+  border-radius: 8px;
+  font-size: 15px;
+  cursor: pointer;
+  transition: background-color 0.8s;
+  margin-top: 20px;
+
+  :hover {
+    background-color: red;
+  }
+`
