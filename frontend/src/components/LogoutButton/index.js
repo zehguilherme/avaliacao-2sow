4eb10606f6ee7cc0ecefb863608993482a674ecc
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-28 00:10:02 -0300

    Uso de estilo de botão do react semantic ui

--- frontend/src/components/LogoutButton/index.js
+++ frontend/src/components/LogoutButton/index.js
@@ -1,15 +1,6 @@
-import styled from 'styled-components';
+import React from 'react'
+import { Button } from 'semantic-ui-react'
 
-export const LogoutButton = styled.button`
-  height: 30px;
-  width: 90px;
-  border-radius: 8px;
-  font-size: 15px;
-  cursor: pointer;
-  transition: background-color 0.8s;
-  margin-top: 20px;
-
-  :hover {
-    background-color: red;
-  }
-`
+export function LogoutButton () {
+  return <Button color='red'>Logout</Button>
+}
