d2d4d33e916109b519b94eb1a3107f1e116e94af
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-29 22:03:39 -0300

    Revert "Mudança do nome do sistema e adição de propriedades usadas nos testes"
    This reverts commit 25a452187c7839f56fd11781a92d96aa8a7bd02f.

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -5,12 +5,12 @@ import { Container } from './styles';
 
 const Navbar = () => {
   return (
-    <Container data-testid="div-container">
-      <h1 data-testid="h1">HubUser</h1>
+    <Container>
+      <h1>Nome do sistema</h1>
 
-      <div className="buttons" data-testid="div.buttons">
-        <Link to="user-list" id="list-user" data-testid="#list-user-link">Listagem</Link>
-        <Link to="insert-edit-user" id="insert-edit-user" data-testid="#insert-edit-user-link">Cadastro/Edição</Link>
+      <div className="buttons">
+        <Link to="user-list" id="list-user">Listagem</Link>
+        <Link to="insert-edit-user" id="insert-edit-user">Cadastro/Edição</Link>
       </div>
     </Container>
   )
